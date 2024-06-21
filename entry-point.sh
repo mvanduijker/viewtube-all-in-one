603a24c48b86f75a188b909d28c1fd7b72dfb649
Mark van Duijker <mark@duyker.nl>
2024-06-21 13:17:01 +0200

    fix: entrypoint

--- entry-point.sh
+++ entry-point.sh
@@ -2,4 +2,4 @@
 mkdir -p /data/redis || true && mkdir -p /data/db || true &&
 redis-server --dir /data/redis/ &
 mongod --dbpath /data/db &
-node /home/app/server/dist/main.cjs
+node /home/app/server/dist/main.js
